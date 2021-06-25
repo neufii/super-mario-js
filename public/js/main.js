a7b98032de81f3f97de8a7eae3564c8a247b51b8
Panida Wiriyachaiporn <neufii.s@gmail.com>
2021-06-25 14:13:32 +0700

    keyboard

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,9 @@ import Entity from './Entity.js';
 import {createMario} from './entities.js'
 import Timer from './Timer.js';
 
+import Keyboard from './KeyboardState.js'
+
+
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -19,18 +22,28 @@ Promise.all([
 	const backgroundLayer = createBackgroundLayer(level.backgrounds,backgroundSprites);
 	comp.layers.push(backgroundLayer);	
 
-	const gravity = 30;
+	const gravity = 2000;
 	mario.pos.set(64,180);
-	mario.vel.set(200,-600);
+
+	const SPACE = 32;
+	const input  = new Keyboard();
+	input.addMapping(SPACE, keyState => {
+		if (keyState) {
+			mario.jump.start();
+		} else {
+			mario.jump.cancel();
+		}
+	})
+	input.listenTo(window);
 
 	const spriteLayer = createSpriteLayer(mario);
 	comp.layers.push(spriteLayer);
 
 	const timer = new Timer(1/60);
 	timer.update = function update(deltaTime){
-		comp.draw(context);
 		mario.update(deltaTime);
-		mario.vel.y += gravity;
+		comp.draw(context);
+		mario.vel.y += gravity * deltaTime;
 	}
 	timer.start();
 });
